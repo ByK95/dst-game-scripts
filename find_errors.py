827d89e31fb7c5d534ff4f6ab225a6a3b9c8dd1c
ByK95 <byrmkaya97@gmail.com>
2023-09-19 10:54:01 +0300

    add helper error script

---
+++ find_errors.py
@@ -0,0 +1,15 @@
+import os
+import re
+
+start_path = "C:\\Users\\bayram\\Documents\\Klei\\DoNotStarveTogether"
+pattern = re.compile(r'LUA ERROR stack traceback:.*', re.DOTALL)
+
+for root, dirs, files in os.walk(start_path):
+    for file in files:
+        if file in ('master_server_log.txt', 'client_log.txt'):
+            with open(os.path.join(root, file), 'r', encoding='utf-8', errors='ignore') as f:
+                content = f.read()
+                import ipdb;ipdb.set_trace()
+                matches = pattern.findall(content)
+                for match in matches:
+                    print(match)
