d6af8935a5b28f0068a810ded71cce78d9f3a6b4
ByK95 <byrmkaya97@gmail.com>
2023-10-16 00:05:02 +0300

    update logs to print logs about mods

--- find_errors.py
+++ find_errors.py
@@ -2,13 +2,19 @@ import os
 import re
 
 start_path = "C:\\Users\\bayram\\Documents\\Klei\\DoNotStarveTogether"
-pattern = re.compile(r'LUA ERROR stack traceback:.*', re.DOTALL)
+error_pattern = re.compile(r'LUA ERROR stack traceback:.*', re.DOTALL)
+mod_init_pattern = re.compile(r'Mod: init[^\n]*', re.DOTALL)
 
 for root, dirs, files in os.walk(start_path):
     for file in files:
         if file in ('master_server_log.txt', 'client_log.txt'):
             with open(os.path.join(root, file), 'r', encoding='utf-8', errors='ignore') as f:
                 content = f.read()
-                matches = pattern.findall(content)
-                for match in matches:
-                    print(match)
+                error_matches = error_pattern.findall(content)
+                mod_init_matches = mod_init_pattern.findall(content)
+                
+                for match in error_matches:
+                    print("Error Match:", match)
+                    
+                for match in mod_init_matches:
+                    print("# Mod: init Match:", match)
