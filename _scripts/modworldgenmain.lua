eaa18b84f2dc19a9b578f618dc7faa5ed186314e
ByK95 <byrmkaya97@gmail.com>
2023-10-20 16:27:48 +0300

    add slot machine to world generation

---
+++ _scripts/modworldgenmain.lua
@@ -0,0 +1,22 @@
+ --import some needed stuff
+ local Layouts = GLOBAL.require("map/layouts").Layouts --this is where we add the setpiece
+ local StaticLayout = GLOBAL.require("map/static_layout") --this helps us load the setpiece
+ 
+ Layouts["slotmachine_layout.lua"] = StaticLayout.Get("map/static_layouts/slotmachine_layout.lua") --index your setpiece
+ 
+ --AddRoomPreInit("Forest", function(room) --"Forest" is the biotope in this case
+ --    if not room.contents.countstaticlayouts then
+ --        room.contents.countstaticlayouts = {}
+ --    end
+ --    room.contents.countstaticlayouts["surface_lightbulb_broken_clockwork_bishop"] = 1 --add one
+ --end)
+ 
+ AddLevelPreInitAny(function(level)
+     if level.location ~= "forest" then -- only in overworld
+         return
+     end
+     if level.required_setpieces == nil then -- if required_setpieces does not exist already, create it
+         level.required_setpieces = {}
+     end
+     table.insert(level.required_setpieces, "slotmachine_layout.lua")
+ end)
--- export_mod.py
+++ export_mod.py
@@ -1,25 +1,33 @@
 import os
 import shutil
 
+def delete_dir_contents(directory):
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+
 def copy_files(source_dir, destination_dir):
-    # Iterate through all files and subdirectories in the source directory
     for root, _, files in os.walk(source_dir):
         for file in files:
             source_path = os.path.join(root, file)
             destination_path = os.path.join(destination_dir, os.path.relpath(source_path, source_dir))
-
-            # Create the directory structure in the destination if it doesn't exist
             os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-
-            # Check if the file in the destination directory exists, and if so, delete it
+            
             if os.path.exists(destination_path):
                 os.remove(destination_path)
-
-            # Copy the file from the source to the destination directory
+            
             shutil.copy2(source_path, destination_path)
 
+# Your script setup
 script_directory = os.path.dirname(os.path.abspath(__file__))
 source_directory = os.path.join(script_directory, '_scripts')
 destination_directory = "D:\\SteamLibrary\\steamapps\\common\\Don't Starve Together\\mods\\init"
 
+# Delete contents of destination directory first
+delete_dir_contents(destination_directory)
+
+# Then copy the files
 copy_files(source_directory, destination_directory)
